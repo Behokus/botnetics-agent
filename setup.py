74c56eeda8148eab5534742ed0e3b21be6f93759
Behokus <behokus@proton.me>
2025-06-22 19:39:41 +0200

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="botnetics-agent",
-    version="0.1.1",
+    version="0.1.2",
     author="Behokus",
     author_email="behokus@proton.me",
     description="Simple framework for building Telegram agents with Django",
