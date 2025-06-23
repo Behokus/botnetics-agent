e4f61c6f18a5834915384d8eb401687e06cfb917
Behokus <behokus@proton.me>
2025-06-23 17:50:05 +0200

    v0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="botnetics-agent",
-    version="0.1.7",
+    version="0.1.8",
     author="Behokus",
     author_email="behokus@proton.me",
     description="Simple framework for building Telegram agents with Django",
